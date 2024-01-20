35188810a3296031ab312538c64ee4c3a983e45b
inoa-mlsilveira <144263612+inoa-mlsilveira@users.noreply.github.com>
2024-01-20 09:32:09 -0300

    parallel std

--- calculate_std_deviation.py
+++ calculate_std_deviation.py
@@ -1,8 +1,11 @@
+import threading
 from src.matchers.depth_distribution_matcher import Depth_Distribution_KL_Matcher
 from src.deviation_calculator import *
 import os
 import json
 from src.utils import file_separator
+from queue import Queue
+from src.job_workers import JobWorkers
 
 SOURCE_FOLDER = os.path.join('data','VisDrone2019-MOT-test-dev')
 DEPTH_SEQUENCE_FOLDER = os.path.join('data', 'depth_track')
@@ -17,7 +20,20 @@ deviation_file = os.path.join("data","standard_deviations.json")
 sequences = [ seq.split('.')[0].split(file_separator())[-1] for seq in os.listdir(os.path.join(calcs[0].source_folder, calcs[0].annotations))]
 content: dict[dict[str,float]]
 test_sequences = ['uav0000009_03358_v','uav0000077_00720_v', 'uav0000120_04775_v', 'uav0000201_00000_v', 'uav0000297_02761_v', 'uav0000119_02301_v']
-
+lock = threading.Lock()
+seq_queue = Queue()
+def iterate_a_sequence(seq: str, args):
+    content, metrics, deviation_file, bar = args
+    std = calc.calculate_for_a_sequence(seq)
+    print(f'Sequence {seq} - Standard Deviation: {std}')
+    lock.acquire()
+    content['standard-deviations'][seq][metrics[i]] = std
+    f = open(deviation_file, "w")
+    json.dump(content, f)
+    f.close()
+    bar.next()
+    lock.release()
+    
 if not os.path.isfile(deviation_file):
     content = {"standard-deviations": {},
                "mean-standard-deviations": {}}
@@ -37,12 +53,9 @@ for i, calc in enumerate(calcs):
             if metrics[i] not in content['standard-deviations'][seq].keys():
                 content['standard-deviations'][seq][metrics[i]] = None
             if content['standard-deviations'][seq][metrics[i]] == None:
-                std = calc.calculate_for_a_sequence(seq)
-                print(f'Sequence {seq} - Standard Deviation: {std}')
-                content['standard-deviations'][seq][metrics[i]] = std
-                f = open(deviation_file, "w")
-                json.dump(content, f)
-                f.close()
+                seq_queue.put(seq)
+        job = JobWorkers(seq_queue, iterate_a_sequence, 2, False, content, metrics, deviation_file)
+        for seq in sequences:
             sum += content['standard-deviations'][seq][metrics[i]]
         mean = sum /len(seq)
         print(f'Mean Standard Deviation: {mean}')
@@ -54,3 +67,4 @@ for i, calc in enumerate(calcs):
             if os.path.isfile(depth_file):
                 shutil.rmtree(depth_file)
         f.close()
+
